d4b889045acce71cffa1c0db8fd3036de8925948
Musa Khan <tamzid834@gmail.com>
2016-12-07 08:50:22 +0600

    initial commit

---
+++ download.h
@@ -0,0 +1,42 @@
+#ifndef DOWNLOAD_H
+#define DOWNLOAD_H
+
+#include <string>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Options.hpp>
+#include <sstream>
+#include <fstream>
+
+std::string download(const std::string url, const std::string filename=""){
+
+    std::ostringstream os;
+    std::string html;
+
+    try{
+
+          os <<  curlpp::options::Url(url);
+
+          html = os.str();
+
+          if(filename != ""){
+              // writting the downloaded data to a file.
+              std::ofstream file;
+              file.open(filename);
+              file << html;
+          }
+
+    }catch(curlpp::RuntimeError& e){
+
+        std::cout << e.what() << "\n";
+        html ="";
+
+    }catch(curlpp::LogicError& e){
+        std::cout << e.what() << "\n";
+        html = "";
+    }
+
+    return html;
+
+}
+
+#endif
